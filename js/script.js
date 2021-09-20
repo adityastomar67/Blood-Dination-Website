6bee054a9e451256c1a9136466a5c0c55d42ee5c
Aditya Singh Tomar <adityastomar12@gmail.com>
2021-09-20 10:29:10 +0530

    Formatted

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
-"use strict"
+'use strict';
 
-const liExplore = document.querySelector(".nav-item-explore");
-const liPlaces = document.querySelector(".nav-item-places");
+const liExplore = document.querySelector('.nav-item-explore');
+const liPlaces = document.querySelector('.nav-item-places');
 
-const exploreModal = document.querySelector(".explore");
-const placesModal = document.querySelector(".places");
+const exploreModal = document.querySelector('.explore');
+const placesModal = document.querySelector('.places');
 
+liExplore.addEventListener('click', function () {
+    placesModal.classList.add('hidden');
+    exploreModal.classList.toggle('hidden');
+});
 
-liExplore.addEventListener("click", function(){
-    placesModal.classList.add("hidden");
-    exploreModal.classList.toggle("hidden");
-})
-
-liPlaces.addEventListener("click", function(){
-    exploreModal.classList.add("hidden");
-    placesModal.classList.toggle("hidden");
-})
+liPlaces.addEventListener('click', function () {
+    exploreModal.classList.add('hidden');
+    placesModal.classList.toggle('hidden');
+});
